d295c126c0ab9c05693632cae0871d66e6e5b370
sam-levin <sam.ky.levin@gmail.com>
2022-05-01 13:52:17 -0700

    changing port

--- server.js
+++ server.js
@@ -23,9 +23,10 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/notes.html'))
 })
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
-  });
+app.listen(process.env.PORT || 5000);
+
+
+
 
 app.post('/api/notes', (req, res) => {
     // req.body is where incoming content will be
