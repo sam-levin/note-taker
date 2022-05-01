3aca23bd38bd75ea1b50c4a970942482cf44ef64
sam-levin <sam.ky.levin@gmail.com>
2022-05-01 13:46:43 -0700

    repushing again

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require ('express');
 const app = express();
-const {data} = require('../develop/db/db.json')
+const {data} = require('./db/db.json')
 const PORT = process.env.PORT || 3001;
 const fs = require('fs');
 const path = require('path');
